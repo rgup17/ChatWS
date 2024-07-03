2ed021af049c9632f29bd4cfebc6be839da29189
rgup17 <rachitgupta428@gmail.com>
2024-07-02 23:55:16 -0400

    implemented ChatMessage and ChatNotification handling.

---
+++ src/main/java/com/chatbot/service/repository/ChatMessageRepository.java
@@ -0,0 +1,10 @@
+package com.chatbot.service.repository;
+
+import com.chatbot.service.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByChatId(String s);
+}
